0066d377fa274767cdab730acfa37e299003d2c9
John Halter <johnhalter@mini1.local>
2022-12-06 13:11:12 -0600

    update

---
+++ cule.sh
@@ -0,0 +1,19 @@
+python3 -m pip install torch
+python3 -m pip install torchvision
+python3 -m pip install psutil 
+python3 -m pip install pytz 
+python3 -m pip install tqdm 
+python3 -m pip install atari_py
+export PATH=/home/coder/.local/bin:$PATH
+
+python3 -m pip install opencv-python
+python3 -m pip install cython
+
+python3 -m pip install --upgrade pip 
+python3 -m pip install --upgrade setuptools
+python3 -m pip install --upgrade wheel 
+
+git clone --recursive https://github.com/NVLabs/cule
+cd cule && python3 -m pip install .
+
+
