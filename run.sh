1e03535e6ee18d5ced2dceb88bb87c14a79df8d8
John Halter <johnhalter@mini1.local>
2022-12-06 20:32:55 -0600

    add parameters

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 cd bin
-python3 a2c_main.py
+python3 a2c_main.py --env-name SpaceInvadersNoFrameskip-v4 --normalize --use-cuda-env --num-ales 1200 --num-steps 20 --num-steps-per-update 1 --num-minibatches 20 --t-max 8000000 --evaluation-interval 200000
